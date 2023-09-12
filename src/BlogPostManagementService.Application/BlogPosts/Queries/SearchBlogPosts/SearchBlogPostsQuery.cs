830a0c4d06bfe4081074be530ed2e9871a885a35
Alexander-Shein <alexa-montana@live.com>
2023-09-12 13:55:35 +0300

    add default values to query

--- src/BlogPostManagementService.Application/BlogPosts/Queries/SearchBlogPosts/SearchBlogPostsQuery.cs
+++ src/BlogPostManagementService.Application/BlogPosts/Queries/SearchBlogPosts/SearchBlogPostsQuery.cs
@@ -1,21 +1,20 @@
 ﻿using BlogPostManagementService.Application.BlogPosts.Queries.SearchBlogPosts.DTOs;
 using EmpCore.QueryStack;
 
-namespace BlogPostManagementService.Application.BlogPosts.Queries.SearchBlogPosts
+namespace BlogPostManagementService.Application.BlogPosts.Queries.SearchBlogPosts;
+
+public class SearchBlogPostsQuery : Query<PagedList<BlogPostListItemDto>>
 {
-    public class SearchBlogPostsQuery : Query<PagedList<BlogPostListItemDto>>
-    {
-        public int PageSize { get; }
-        public int PageNumber { get; }
-        public string SortField { get; }
-        public SortDir SortDir { get; }
+    public int PageSize { get; } = 100;
+    public int PageNumber { get; } = 1;
+    public string SortField { get; } = nameof(BlogPostListItemDto.CreatedAt);
+    public SortDir SortDir { get; } = SortDir.Desc;
         
-        public SearchBlogPostsQuery(int pageSize, int pageNumber, string sortField, SortDir sortDir)
-        {
-            PageSize = pageSize;
-            PageNumber = pageNumber;
-            SortField = sortField;
-            SortDir = sortDir;
-        }
+    public SearchBlogPostsQuery(int? pageSize, int? pageNumber, string? sortField, SortDir? sortDir)
+    {
+        if (pageSize != null) PageSize = pageSize.Value;
+        if (pageNumber != null) PageNumber = pageNumber.Value;
+        if (sortField != null) SortField = sortField;
+        if (sortDir != null) SortDir = sortDir.Value;
     }
 }
--- src/BlogPostManagementService.WebApi/BlogPosts/Models/SearchBlogPostsInputModel.cs
+++ src/BlogPostManagementService.WebApi/BlogPosts/Models/SearchBlogPostsInputModel.cs
@@ -4,8 +4,8 @@ namespace BlogPostManagementService.WebApi.BlogPosts.Models;
 
 public class SearchBlogPostsInputModel
 {
-    public int PageSize { get; set; }
-    public int PageNumber { get; set; }
-    public string SortField { get; set; }
-    public SortDir SortDir { get; set; }
+    public int? PageSize { get; set; }
+    public int? PageNumber { get; set; }
+    public string? SortField { get; set; }
+    public SortDir? SortDir { get; set; }
 }
